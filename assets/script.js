516129390c39a6562e5c45e06a6e824106dadd3f
Alister Porteous <alisterporteous@hotmail.com>
2023-02-02 06:55:30 -0500

    created autocomplete js, created autocomplete css, added autocomplete links to index html

--- assets/script.js
+++ assets/script.js
@@ -2,20 +2,16 @@
 const publicKey = "14d27cc0e17db4c46e2a1c8c038702f8";
 const privateKey = "dd786203735e0d58df194e5f71e0a50fbc7df72a";
 const apiUrl = "https://gateway.marvel.com:443/v1/public/characters?";
+
+
 //Name "starts with" url = "https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=";
 
-// let outer_container = document.getElementById("outer_container");
-// let thumbnail_container = document.getElementsByClassName(
-//   "thumbnail_container"
-// )[0];
-// let right_container = document.getElementsByClassName("right_container")[0];
-
 //fetch data from Marvel server
-function fetchData(name) {
+function fetchData(nameStartsWith) {
   const ts = Date.now();
   console.log(ts);
   const hash = CryptoJS.MD5(ts + privateKey + publicKey).toString();
-  const url = `${apiUrl}ts=${ts}&apikey=${publicKey}&hash=${hash}&name=${name}`;
+  const url = `${apiUrl}ts=${ts}&apikey=${publicKey}&hash=${hash}&nameStartsWith=${nameStartsWith}`;
   console.log(url);
 
   //returns a promise that fetches the data from the url, processes json and updates page elements
@@ -37,6 +33,36 @@ function fetchData(name) {
     
 }
 
+
+
+
+// Define an array of Marvel hero characters
+// const characters = [
+//   { name: "Iron Man"},
+//   { name: "Captain America"},
+//   { name: "Thor"},
+//   { name: "Hulk"},
+//   { name: "Black Widow"},
+// ];
+
+// Get a reference to the search input element
+const searchInput = document.getElementById("heroName");
+
+// Attach an event listener to the search input
+searchInput.addEventListener("input", function() {
+  // Get the search term from the search input
+  const searchTerm = searchInput.value.toLowerCase();
+
+  // Filter the characters array based on the search term
+  const filteredCharacters = characters.filter(function(character) {
+    return (
+      character.name.toLowerCase().indexOf(searchTerm) !== -1);
+  });
+
+  // Log the filtered characters array to the console
+  console.log(filteredCharacters);
+});
+
 // response from the API is stored in a variable named 'data'
 //const hero = ch_data.data.results[0];
 
@@ -46,23 +72,7 @@ function fetchData(name) {
 // display the information on the page
 
 //ids for name, description and image
-// return fetch(url)
-//   .then((response) => {
-//     if (response.ok) {
-//       return response.json();
-//     }
-//     throw new Error("Network response was not ok.");
-//   })
 
-//     .then(data => {
-//       const response = data.data.results[0];
-//       const nameElement = document.getElementById("name");
-//       nameElement.textContent = response.name;
-//       const descriptionElement = document.getElementById("description");
-//       descriptionElement.textContent = response.description;
-//       const imageElement = document.getElementById("image");
-//       imageElement.src = `${response.thumbnail.path}.${response.thumbnail.extension}`;
-//     })
 //     .catch(error => console.log("There was a problem with the fetch operation: ", error));
 // }
 
@@ -72,38 +82,3 @@ document.getElementById("button").addEventListener("click", function (event) {
   fetchData(name);
   event.preventDefault();
 });
-
-//document.addEventListener('DOMContentLoaded', submitButton)
-//var apiKey="14d27cc0e17db4c46e2a1c8c038702f8"
-//var pKey="dd786203735e0d58df194e5f71e0a50fbc7df72a"
-//var hash = md5(number + pKey + apiKey);
-//function submitButton() {
-//add a timestamp to the request
-//var number = Date.now()
-//var req = new XMLHttpRequest();
-//var apiUrl = 'https://gateway.marvel.com/v1/public/characters?name='
-//var name = document.getElementById('heroName').value;
-//var heroNameData = apiUrl + name + '&ts=' + number + '&apikey=' + apiKey + '&hash=' + pKey;
-
-//document.getElementById('submit').addEventListener('click', function(event) {
-//  document.getElementById('heroName').textContent = "";
-// document.getElementById('description').textContent = "";
-// document.getElementById('image').textContent = "";
-//  });
-// req.open('GET', heroNameData, true);
-
-// req.setRequestHeader('Content-Type', 'application/json');
-//  req.addEventListener('load', function() {
-//    if (req.status >= 200 && req.status < 400) {
-//        var response = JSON.parse(req.responseText);
-//        console.log(response);
-//       document.getElementById('name').textContent = response.data.results[0].name;
-//      document.getElementById('description').textContent = response.data.results[0].description;
-//       document.getElementById('image').textContent = response.data.results[0].thumbnail.path + '.' + response.data.results[0].thumbnail.extension;
-//  } else {
-//       console.log("Error in network request: " + req.statusText);
-//  }
-//   event.preventDefault();
-//  });
-//   req.send(null);
-//};
